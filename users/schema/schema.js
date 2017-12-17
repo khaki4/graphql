bbccaea5770ac560a0e80dfe7c9f18823488d953
정지원 <4dakto@gmail.com>
2017-12-17 22:57:11 +0900

    Jiwon :: fetch from json-server

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql');
+const axios = require('axios');
 const _ = require('lodash');
 const {
   GraphQLObjectType,
@@ -27,7 +28,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(({ data }) => data);
       }
     }
   }
