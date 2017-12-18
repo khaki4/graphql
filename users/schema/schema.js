72167c4dcfecf53babe1300cbb702bdb7f545544
Jiwon Jung <jw.jeoung@albamapp.com>
2017-12-18 14:15:48 +0900

    add mutations

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -76,6 +76,29 @@ const mutation = new GraphQLObjectType({
         return axios.post('http://localhost:3000/users', { firstName, age })
           .then(({ data }) => data);
       }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/users/${id}`)
+          .then(({ data }) => data);
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+          .then(({ data }) => data);
+      }
     }
   }
 });
