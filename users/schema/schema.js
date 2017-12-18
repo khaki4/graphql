d1de7be05e982be8db504035be4109b200d1272b
Jiwon Jung <jw.jeoung@albamapp.com>
2017-12-18 12:15:55 +0900

    Bidirectional Relations

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -5,18 +5,25 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(({ data }) => data);
+      }
+    }
+  })
 });
-
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
